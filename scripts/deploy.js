b4604f10286c53cdd32c78175cde962b0865b151
blackalbino01 <hauwal4969@gmail.com>
2023-08-16 01:43:11 +0500

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const uniswapRouterAddress = "";
-  const usdcTokenAddress = "";
+  const uniswapRouterAddress = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
+  const usdcTokenAddress = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
 
   const Swap = await hre.ethers.getContractFactory("Swap");
   const swap = await Swap.deploy(uniswapRouterAddress, usdcTokenAddress);
 
   await swap.deployed();
 
-  console.log("Swap deployed to:", swap.address);
+  console.log("Swap Contract deployed to:", swap.address);
 }
 
 main().catch((error) => {
